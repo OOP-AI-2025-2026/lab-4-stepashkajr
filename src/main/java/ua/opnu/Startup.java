d62ac6df794c73a4bb014fa385d40a6f7c33c2b4
stepashkajr <sergey.stepanenko.junior@gmail.com>
2025-12-02 15:06:36 +0100

    Create Startup.java

---
+++ src/main/java/ua/opnu/Startup.java
@@ -0,0 +1,13 @@
+package ua.opnu.java.inheritance.account;
+
+public class Startup {
+    private int initialBalance;
+
+    public Startup(int initialBalance) {
+        this.initialBalance = initialBalance;
+    }
+
+    public int getInitialBalance() {
+        return initialBalance;
+    }
+}
